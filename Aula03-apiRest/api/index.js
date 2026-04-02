6fade0abee5aa05a8ed7f545c04537bc5efd55d7
85881295072 <85881295072@senacrs.edu.br>
2026-04-02 12:04:05 -0300

    aula dia 02/04

--- Aula03-apiRest/api/index.js
+++ Aula03-apiRest/api/index.js
@@ -4,6 +4,10 @@ const errors = require("http-errors")
 
 const app = express()
 
+const cors = require("cors")
+
+app.use( cors() )
+
 app.use( express.json() )
 app.use( express.urlencoded( {extended : true} ) )
 
